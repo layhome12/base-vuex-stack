41d5e0b3c4cc47f16c3752fed26f20d6a968a97b
layhome12 <ilhamnm12@gmail.com>
2026-05-29 18:38:51 +0700

    fix: group_access table

--- backend/src/database/migrations/1706668580002_create_group_access_table.ts
+++ backend/src/database/migrations/1706668580002_create_group_access_table.ts
@@ -2,14 +2,34 @@ import { Knex } from "knex";
 
 export async function up(knex: Knex): Promise<void> {
   await knex.schema.createTable("group_access", (table) => {
-    table.string("group_id", 30).primary();
-    table.string("sidebar_key", 30).primary();
-    table.boolean('read').defaultTo(false);
-    table.boolean('create').defaultTo(false);
-    table.boolean('update').defaultTo(false);
-    table.boolean('delete').defaultTo(false);
+    table.integer("group_id").unsigned().notNullable();
+    table.string("sidebar_key", 30).notNullable();
+
+    table.boolean("read").defaultTo(false);
+    table.boolean("create").defaultTo(false);
+    table.boolean("update").defaultTo(false);
+    table.boolean("delete").defaultTo(false);
     table.dateTime("created_at").nullable();
     table.dateTime("updated_at").nullable();
+
+    // UNIQUE
+    table.unique(["group_id", "sidebar_key"]);
+
+    // FK -> groups.group_id
+    table
+      .foreign("group_id")
+      .references("id")
+      .inTable("groups")
+      .onDelete("CASCADE")
+      .onUpdate("CASCADE");
+
+    // FK -> sidebars.key
+    table
+      .foreign("sidebar_key")
+      .references("key")
+      .inTable("sidebars")
+      .onDelete("CASCADE")
+      .onUpdate("CASCADE");
   });
 }
 
